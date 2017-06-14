4e63f5d4148f863875dc97250a7289268c0190a7
Jeff Escalante <escalantejeff@gmail.com>
2017-06-14 18:51:39 -0400

    implement rehydration

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,11 @@
 import 'preact/devtools'
 import MyComponent from './my-component'
 import {h, render} from 'preact'
+import {hydrateInitialState} from 'reshape-preact-components/lib/browser'
 
-render(
-  <MyComponent name='BrooklynJS' />,
-  document.body,
-  document.querySelector('.my-component')
-)
+const el = document.querySelector('.my-component')
+const vdom = hydrateInitialState(el.dataset.state, {
+  'my-component': MyComponent
+})
+
+render(vdom, document.body, el)
--- assets/my-component.js
+++ assets/my-component.js
@@ -3,7 +3,7 @@ const {h, Component} = require('preact')
 module.exports = class MyComponent extends Component {
   render () {
     return (
-      <p className='my-component'>
+      <p className='my-component' data-state={this.props._state}>
         Hello {this.props.name}! 👋
       </p>
     )
